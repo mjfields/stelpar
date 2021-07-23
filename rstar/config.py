7afd10212df3381e0b3f73d455faf1efe4372907
Matt Fields <55111366+mjfields@users.noreply.github.com>
2021-07-23 15:21:58 -0400

    Add files via upload

--- rstar/config.py
+++ rstar/config.py
@@ -9,18 +9,18 @@ import os
 
 
 
-MAGMODELPATH = os.path.abspath(os.path.join(os.path.dirname(__file__), r'resources/feiden_grid_mag.csv'))
+MAGMODELPATH = os.path.abspath(os.path.join(os.path.dirname(__file__), r'resources/feiden_grid_mag.csv.gz'))
 
-INTERPMAGMODELPATH = os.path.abspath(os.path.join(os.path.dirname(__file__), r'resources/feiden_grid_mag_interpolated.csv'))
+INTERPMAGMODELPATH = os.path.abspath(os.path.join(os.path.dirname(__file__), r'resources/feiden_grid_mag_interpolated.csv.gz'))
 
-STDMODELPATH = os.path.abspath(os.path.join(os.path.dirname(__file__), r'resources/feiden_grid_std.csv'))
+STDMODELPATH = os.path.abspath(os.path.join(os.path.dirname(__file__), r'resources/feiden_grid_std.csv.gz'))
 
-INTERPSTDMODELPATH = os.path.abspath(os.path.join(os.path.dirname(__file__), r'resources/feiden_grid_std_interpolated.csv'))
+INTERPSTDMODELPATH = os.path.abspath(os.path.join(os.path.dirname(__file__), r'resources/feiden_grid_std_interpolated.csv.gz'))
 
-PARSECMODELPATH = os.path.abspath(os.path.join(os.path.dirname(__file__), r'resources/parsec_grid.csv'))
+PARSECMODELPATH = os.path.abspath(os.path.join(os.path.dirname(__file__), r'resources/parsec_grid.csv.gz'))
 
 GRIDCACHE = os.path.abspath(os.path.join(os.path.dirname(__file__), r'resources/.grid_cache'))
 
 FILTERPROFILESPATH = os.path.abspath(os.path.join(os.path.dirname(__file__), r'resources/Filter_Profiles'))
 
-REMOTEHOMEPATH = os.path.abspath(os.path.join(os.path.dirname(__file__), r'..'))
+# REMOTEHOMEPATH = os.path.abspath(os.path.join(os.path.dirname(__file__), r'..'))
