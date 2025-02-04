04b3f4c590d888c4eae10ee3e35e237f39a6881e
mjfields <fieldsmatthewj@gmail.com>
2025-02-04 12:13:33 -0500

    fixed path

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('../../src/stelpar'))
+sys.path.insert(0, os.path.abspath(os.path.join('..', '..', 'src', 'stelpar')))
 
 # -- Project Information -----------------------------------------------------
 
