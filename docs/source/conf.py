f3a020b69c618febf41a90998a13d5c3afa084bb
mjfields <fieldsmatthewj@gmail.com>
2025-02-04 13:38:09 -0500

    edit

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,8 +1,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath(os.path.join('..', '..', 'src')))
-print(sys.path)
+sys.path.insert(0, os.path.abspath(os.path.join('..', '..')))
 
 # -- Project Information -----------------------------------------------------
 
